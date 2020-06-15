ed15469be66dde7070a44412f333a9cb4b65b469
Brian Fontaine <brian02440@live.fr>
2020-06-15 09:55:50 +0200

    tp partie 9 terminer erreur corriger

--- TP/index.php
+++ TP/index.php
@@ -41,7 +41,7 @@
         </select>
         <select class="form-control form-control-sm col-3 mt-2 ml-2" name="years" value="Years" id="years">
             <?php 
-    for ($years=1970; $years < 2021 ; $years++)  
+    for ($years=1500; $years < 2021 ; $years++)  
     {
         echo '<option value="'.$years.'">'.$years.'</option>';    
     }
@@ -71,6 +71,7 @@
             $w3 = 1;
             $w4 = 1;
             $w5 = 1;
+            $w6 = 1;
             
                 for ($i = 1; $i <= cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']); $i++) 
             {
@@ -481,13 +482,253 @@
                                 $primaryDays++;
                                 $w5++;
                             }
-
                         }
-                    
-
+                    }
+                }
+                if ($nameDays == 'Wed')
+                {
+                    if ($i >= 26 && $i <= cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) )
+                    {
+                    $primaryDays = 27;
+                        if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 28)
+                        {
+                            while ($w5 <= 2 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 29)
+                        {
+                            while ($w5 <= 3 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }
+                        else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) != 31)
+                        {
+                            while ($w5 <= 4 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }else 
+                        {
+                            while ($w5 <= 5 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }
+                    }
+                }
+                if ($nameDays == 'Thu')
+                {
+                    if ($i >= 25 && $i <= cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) )
+                    {
+                    $primaryDays = 26;
+                        if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 28)
+                        {
+                            while ($w5 <= 3 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 29)
+                        {
+                            while ($w5 <= 4 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }
+                        else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) != 31)
+                        {
+                            while ($w5 <= 5 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }else 
+                        {
+                            while ($w5 <= 6 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }                  
+                    }
+                }
+                if ($nameDays == 'Fri')
+                {
+                    if ($i >= 24 && $i <= cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) )
+                    {
+                    $primaryDays = 25;
+                        if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 28)
+                        {
+                            while ($w5 <= 4 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 29)
+                        {
+                            while ($w5 <= 5 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }
+                        else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) != 31)
+                        {
+                            while ($w5 <= 6 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }else 
+                        {
+                            while ($w5 <= 7 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }
+                    }
+                }
+                if ($nameDays == 'Sat')
+                {
+                    if ($i >= 23 && $i <= cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) )
+                    {
+                    $primaryDays = 24;
+                        if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 28)
+                        {
+                            while ($w5 <= 5 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 29)
+                        {
+                            while ($w5 <= 6 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }
+                        else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 30)
+                        {
+                            while ($w5 <= 7 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }else if (cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) )
+                        {
+                        while ($w5 <= 7) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }
+                    }
+                }
+                if ($nameDays == 'Sun')
+                {
+                    if ($i >= 22 && $i <= cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) )
+                    {
+                    $primaryDays = 23;
+                        if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 28)
+                        {
+                            while ($w5 <= 6 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 29)
+                        {
+                            while ($w5 <= 7 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }
+                        else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 30)
+                        {
+                            while ($w5 <= 7 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }
+                        else if ( cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 31)
+                        {
+                            while ($w5 <= 7 ) 
+                            {
+                                echo $message = "<td class='text-center'>".$primaryDays."</td>";
+                                $primaryDays++;
+                                $w5++;
+                            }
+                        }
                     }
                 }     
             } ?>
+                </tr>
+                <tr>
+                <?php
+                if ($nameDays == 'Sat')
+                {
+                if (cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 31)
+                        {
+                        while ($w6 <= 1) 
+                            {
+                                echo $message = "<td class='text-center'>31</td>";
+                                $primaryDays++;
+                                $w6++;
+                            }
+                        }
+                    }
+                    if ($nameDays == 'Sun')
+                    {
+                        if ($i >= 29 && cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 30)
+                        {
+                            while ($w6 <= 1)
+                            {
+                                echo $message = "<td class='text-center'>30</td>";
+                                echo $message = "<td class='text-center'>31</td>";
+                                $w6++;
+                            }
+                        }
+                        if ($i >= 29 && cal_days_in_month(CAL_GREGORIAN, $_GET['months'], $_GET['years']) == 31)
+                        {
+                        while ($w6 <= 1) 
+                        {
+                            echo $message = "<td class='text-center'>30</td>";
+                            echo $message = "<td class='text-center'>31</td>";
+                            $w6++;
+                        }
+                }
+            }
+        ?> 
                 </tr>
             </tbody>
         </table>
